1464005678f601f67a379214f512c87f61b5ad22
David Melhart <david.melhart@gmail.com>
2016-12-13 17:00:24 +0100

    SpriteGameObject added

--- elements/GameObject.js
+++ elements/GameObject.js
@@ -1,49 +1,28 @@
 "use strict";
 
 function GameObject () {
-	this.sprite = undefined;
-	this.origin = Vector2.zero;
 	this.position = Vector2.zero;
-	this.rotation = 0;
 	this.velocity = Vector2.zero;
 	this.visible = true
+	this.parent = null;
+	this.layer = 0;
 }
 
+Object.defineProperty(GameObject.prototype, 'worldPosition', 
+	{
+		get: function () {
+			if (this.parent != null) {
+				return this.parent.worldPosition.addTo(this.position);
+			} else{
+				return this.position.copy();
+			}
+		}
+	});
+
 GameObject.prototype.draw = function () {
-	if (!this.visible) {
-		return;
-	} else {
-		Canvas2D.drawImage(this.sprite, this.position, this.rotation, this.origin)
-	}
+
 };
 
 GameObject.prototype.update = function (delta) {
 	this.position.addTo(this.velocity.multiply(delta));
 };
-
-Object.defineProperty(GameObject.prototype, 'center', {
-		get: function () {
-			return new Vector2(this.sprite.width/2, this.sprite.height/2);
-		}
-	});
-
-Object.defineProperty(GameObject.prototype, 'width',
-    {
-        get: function () {
-            return this.sprite.width;
-        }
-    });
-
-Object.defineProperty(GameObject.prototype, 'height',
-    {
-        get: function () {
-            return this.sprite.height;
-        }
-    });
-
-Object.defineProperty(GameObject.prototype, 'size',
-    {
-        get: function () {
-            return new Vector2(this.sprite.width, this.sprite.height);
-        }
-    });
---
+++ elements/SpriteGameObject.js
@@ -0,0 +1,47 @@
+"use strict";
+
+function SpriteGameObject (sprite, layer, scale) {
+	layer = typeof layer !== 'undefined' ? layer : 0;
+	scale = typeof scale !== 'undefined' ? scale : 1;
+	GameObject.call(this, layer);
+	
+	this.sprite = sprite;
+	this.origin = Vector2.zero;
+	this.rotation = 0;
+	this.scale = scale;
+}
+
+Object.defineProperty(SpriteGameObject.prototype, 'center', {
+		get: function () {
+			return new Vector2(this.sprite.width/2, this.sprite.height/2);
+		}
+	});
+
+Object.defineProperty(SpriteGameObject.prototype, 'width',
+    {
+        get: function () {
+            return this.sprite.width;
+        }
+    });
+
+Object.defineProperty(SpriteGameObject.prototype, 'height',
+    {
+        get: function () {
+            return this.sprite.height;
+        }
+    });
+
+Object.defineProperty(SpriteGameObject.prototype, 'size',
+    {
+        get: function () {
+            return new Vector2(this.sprite.width, this.sprite.height);
+        }
+    });
+
+SpriteGameObject.prototype.draw = function () {
+	if (!this.visible && this.sprite === undefined) {
+		return;
+	} else {
+		Canvas2D.drawImage(this.sprite, this.worldPosition, this.rotation, this.scale, this.origin)
+	}
+};
