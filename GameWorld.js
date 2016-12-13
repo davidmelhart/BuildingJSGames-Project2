e501576db21a7dd00b9f4906fddd6f20bb1e310a
David Melhart <david.melhart@gmail.com>
2016-12-13 14:54:57 +0100

    "Column" has the letter "N" damn it!

--- GameWorld.js
+++ GameWorld.js
@@ -18,9 +18,22 @@ JewelGameWorld.prototype.getGridValue = function (x, y) {
 };
 
 JewelGameWorld.prototype.setGridValue = function (x, y, value) {
-    var index = y * this.colums + x;
+    var index = y * this.columns + x;
     this.grid[index] = value;
 };
+
+JewelGameWorld.prototype.moveRowDown = function () {
+    for (var y = this.rows - 2; y >= 0; y--) {
+        for (var x = 0; x < this.columns ; x++) {
+            this.setGridValue(x, y + 1, this.getGridValue(x,y));
+        }
+    }
+
+    for (var x = 0; x < this.columns; x++) {
+       var random = Math.floor(Math.random()*3) + 1;
+       this.setGridValue(x, 0, sprites["singleJewel" + random]); 
+    }
+}
 
 JewelGameWorld.prototype.handleInput = function (delta) {
 
